8e752aa7028177c9ba01f5b70e452a1ff37913cb
Elizabeth Alcalá Guillén <elizabeth.alcala@enfasys-tech.com>
2020-10-15 13:30:23 -0500

    Header

---
+++ src/components/layout/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <img src="images/logo.png" alt="todolist"/>
+                </div>
+                <div>
+                    <ul>
+                        <li>A</li>
+                        <li>B</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+}
